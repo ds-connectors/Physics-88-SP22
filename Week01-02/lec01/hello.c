de08184b0f190883ef1626500ffc372b0e7c231f
Yury <ygkolomensky@lbl.gov>
2022-01-18 13:59:58 -0800

    Week01-02

---
+++ Week01-02/lec01/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello, World!\n");
+  printf("Hello again, World!\n");
+
+  int i = 5;
+
+  float x = 3.14;
+  
+  return 11;
+}
+
---
+++ Week01-02/lec01/hello.csh
@@ -0,0 +1,7 @@
+#!/bin/csh -f
+
+echo "Hello world!"
+
+echo "Hello again"
+
+echo "Hello Physics 77"
